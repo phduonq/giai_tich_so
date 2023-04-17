84689c360424b5943a25f4782e620bb037ddd521
pham_duong <phduong.8403@gmail.com>
2023-04-17 23:07:02 +0700

    add comment for bisection method

--- bisection_method.java
+++ bisection_method.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 
 public class bisection_method {
-	static double log(double a, double b) { // tinh logarit a cua b 
+	static double log(double a, double b) { // Tinh logarit a cua b 
 		return Math.log(a) / Math.log(b);
 	}
-	static double f(double x) {
+	static double f(double x) { // Khoi tao ham f(x)
 		double f = Math.pow(2, x) + x - 4;
 		return f;
 	}
 	public static void main(String[] args) {
 		Scanner myScan = new Scanner(System.in);
 		double a, b, e, x;
-		System.out.println("Nhap a: ");
+		// Nhap khoang (a, b)
+		System.out.println("Nhap a: "); 
 		a = myScan.nextDouble();
 		System.out.println("Nhap b: ");
 		b = myScan.nextDouble();
+		// Nhap sai so mong muon
 		System.out.println("Nhap sai so e: ");
 		e = myScan.nextDouble();
 		myScan.close();
 		int n = 0;
 		while(!(b - a < e)) {
-			x = (a + b) / 2;
+			x = (a + b) / 2; // gan x la diem nam giua khoang (a,b)
 			System.out.printf("%d %f %f%n", n, a, b);
-			if(f(a)*f(x) > 0) {
+			if(f(a)*f(x) > 0) { // cap nhat khoang moi
 				a = x;
 			} else {
 				b = x;
